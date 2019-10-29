192393db75b0e35adcf051ab96a65724cedd63ba
Student <example@example.com>
2019-10-29 11:53:00 -0700

    Add for loop function to card deck.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
+var cardSuits = ["hearts", "diamonds", "spades", "clubs"];
+var cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "queen", "king"];
+
 $(document).ready(function() {
 
-
-
-});
+    cardSuits.forEach(function(cardSuit) {
+      cards.forEach(function(card) {
+        $("#output").append("<li>" + card + " of " + cardSuit + "</li>")
+      });
+    });
+  });
